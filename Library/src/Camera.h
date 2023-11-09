e400fa51da47963373fb85ff3ab985dedca49fde
Xander <xander.berten@student.howest.be>
2023-11-09 14:58:18 +0100

    Implemented A moving Camera.h

--- Library/src/Camera.h
+++ Library/src/Camera.h
@@ -17,8 +17,7 @@ namespace dae
 			fovAngle{_fovAngle}
 		{
 		}
-
-
+		
 		Vector3 origin{};
 		float fovAngle{90.f};
 		float fov{ tanf((fovAngle * TO_RADIANS) / 2.f) };
@@ -73,10 +72,58 @@ namespace dae
 
 		void Update(Timer* pTimer)
 		{
-			const float deltaTime = pTimer->GetElapsed();
+						const float deltaTime = pTimer->GetElapsed();
 
-			//Camera Update Logic
-			//...
+			// Keyboard Input
+			const uint8_t* pKeyboardState = SDL_GetKeyboardState(nullptr);
+
+			// Mouse Input
+			int mouseX{}, mouseY{};
+			const uint32_t mouseState = SDL_GetRelativeMouseState(&mouseX, &mouseY);
+
+			// Speed and limit constants
+			constexpr float keyboardMovementSpeed{ 10.0f };
+			constexpr float mouseMovementSpeed{ 2.0f };
+			constexpr float angularSpeed{ 50.0f * TO_RADIANS };
+
+			// The total movement of this frame
+			Vector3 direction{};
+
+			// Calculate new position with keyboard inputs
+			direction += (pKeyboardState[SDL_SCANCODE_W] || pKeyboardState[SDL_SCANCODE_Z]) * forward * keyboardMovementSpeed * deltaTime;
+			direction -= pKeyboardState[SDL_SCANCODE_S] * forward * keyboardMovementSpeed * deltaTime;
+			direction -= (pKeyboardState[SDL_SCANCODE_Q] || pKeyboardState[SDL_SCANCODE_A]) * right * keyboardMovementSpeed * deltaTime;
+			direction += pKeyboardState[SDL_SCANCODE_D] * right * keyboardMovementSpeed * deltaTime;
+
+			// Calculate new position and rotation with mouse inputs
+			switch (mouseState)
+			{
+			case SDL_BUTTON_LMASK: // LEFT CLICK
+				direction -= forward * (mouseY * mouseMovementSpeed * deltaTime);
+				totalYaw += mouseX * angularSpeed * deltaTime;
+				break;
+			case SDL_BUTTON_RMASK: // RIGHT CLICK
+				totalYaw += mouseX * angularSpeed * deltaTime;
+				totalPitch -= mouseY * angularSpeed * deltaTime;
+				break;
+			case SDL_BUTTON_X2: // BOTH CLICK
+				direction.y -= mouseY * mouseMovementSpeed * deltaTime;
+				break;
+			}
+			totalPitch = std::clamp(totalPitch, -89.f * TO_RADIANS, 89.f * TO_RADIANS);
+
+
+			//Movement calculations
+			constexpr float shiftSpeed{ 4.0f };
+			direction *= 1.0f + pKeyboardState[SDL_SCANCODE_LSHIFT] * (shiftSpeed - 1.0f);
+
+			origin += direction;
+
+			// Calculate the rotation matrix with the new pitch and yaw
+			const Matrix rotationMatrix = Matrix::CreateRotationX(totalPitch) * Matrix::CreateRotationY(totalYaw);
+
+			// Calculate the new forward vector with the new pitch and yaw
+			forward = rotationMatrix.TransformVector(Vector3::UnitZ);
 
 			//Update Matrices
 			CalculateViewMatrix();
